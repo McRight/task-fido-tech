6aa065b066f334dd7916b3b1c79160b725629a28
Robert Sanins <robertsanins@gmail.com>
2024-10-21 14:48:03 +0100

    add coordinates to the Bicester office

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,6 +6,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiZXdvbmctZmlkbyIsImEiOiJjbTJkMmkyc2sxMzUwMmpxeGI2dDdndHN6In0.O2rAz61j2wq0fYrWU-2ULA";
 
 function Map({ data }: { data: any[] }) {
+  // const coords = [-1.15692, 51.92066]; // Bicester office
   return <div className="map" />;
 }
 
