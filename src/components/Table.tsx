d44da0c632d97e1625f82e7a44a259049819d55e
Yurii Zaporozhchenko <yurnero017@gmail.com>
2024-10-27 19:25:59 +0000

    2. plot the data into the table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,11 @@
-function Table({ data }: { data: any[] }) {
+// Types
+import { ProcessedData } from '../types';
+
+type TableProps = { data: ProcessedData[] };
+
+// * NB: don't think it's worth making the headers dynamic in this case, since the "data" is already typed and I would use a library for handling the table anyway (something like "TanStack Table" which I have experience using)
+
+function Table({ data }: TableProps) {
   return (
     <table>
       <thead>
@@ -11,13 +18,15 @@ function Table({ data }: { data: any[] }) {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>placeholder</td>
-          <td>placeholder</td>
-          <td>placeholder</td>
-          <td>placeholder</td>
-          <td>placeholder</td>
-        </tr>
+        {data.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.notes}</td>
+            <td>{p.engineerId}</td>
+            <td>{p.coords[0]}</td>
+            <td>{p.coords[1]}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
